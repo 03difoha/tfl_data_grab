8b8c817cc534667a089039d2d959bca4b33b8635
Harry Difolco <h@difol.co>
2020-03-11 07:43:01 +0000

    initial test

---
+++ dataGrab.js
@@ -0,0 +1,75 @@
+require("dotenv").config();
+
+const request = require("request");
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+function saveToDb(body) {
+  for (i of body) {
+    console.log(i);
+    if (!i.name) {
+      return console.log("request bounced at captcha");
+    }
+    table = i.name
+      .slice(0, -6)
+      .replace(/ /g, "_")
+      .replace("&", "and");
+    for (b of i.bays) {
+      if (b.bayType == "Disabled") {
+        dis_bays = b.bayCount;
+        occ_dis_bays = b.occupied;
+      }
+      if (b.bayType == "Pay and Display Parking") {
+        reg_bays = b.bayCount;
+        occ_reg_bays = b.occupied;
+      }
+    }
+
+    d = new Date();
+    d = d.toISOString().split("T")[0] + " " + d.toTimeString().split(" ")[0];
+
+    // sql = `CREATE TABLE ${table} (date DATETIME, reg_bays INT, occ_reg_bays INT, dis_bays INT, occ_dis_bays INT)`;
+
+    sql = `INSERT INTO ${table} (date, reg_bays, occ_reg_bays, dis_bays, occ_dis_bays) VALUES (NOW(), ${reg_bays}, ${occ_reg_bays}, ${dis_bays}, ${occ_dis_bays})`;
+
+    con.query(sql, function(err, result) {
+      if (err) throw err;
+      console.log("record inserted");
+    });
+  }
+}
+
+function refreshData() {
+  x = 900; // 900 seconds = 15 minutes
+
+  request(
+    `https://api.tfl.gov.uk/Occupancy/CarPark?app_id=${process.env.TFL_API_ID}&app_key=${process.env.TFL_API_KEY}`,
+    {
+      json: true
+    },
+    (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      if (body == String) {
+        return body;
+      }
+      saveToDb(body);
+    }
+  );
+
+  setTimeout(refreshData, x * 1000);
+}
+
+refreshData();
